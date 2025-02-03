f12e7a070231c081bf73c17d1105ca2e6813db4b
Puneeth <puneethkumar375@gmail.com>
2025-02-03 09:58:21 +0530

    2nd modified

---
+++ src/attendance.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function attendance() {
+  return (
+    <h3>attendance</h3>
+  )
+}
---
+++ src/classs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function classs() {
+  return (
+    <h2>classs</h2>
+  )
+}
